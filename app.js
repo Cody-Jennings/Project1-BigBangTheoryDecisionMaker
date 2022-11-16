02424e7e6b06b465f0c534bb564cba1e8e491655
Cody Jennings <cojack91@gmail.com>
2022-11-16 18:07:38 -0500

    cleaned up code

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-let playerScore = 0;
+let playerScore = 0;                            //Global Variables
 let shelBotScore = 0;
 let boutStatusMessage1 = "You win! :)";
 let boutStatusMessage2 = "You lose. :(";
 let boutStatusMessage3 = "You tied. :|";
+let gameOver = false;
 
-document.getElementById("rock").onclick = playRock;       //click event listeners for each player choice button
+document.getElementById("rock").onclick = playRock;       //The onclick event attribute works when the user clicks  on the button. When the mouse is clicked on the element then the script runs.
 document.getElementById("paper").onclick = playPaper;
 document.getElementById("scissors").onclick = playScissors;
 document.getElementById("lizard").onclick = playLizard;
 document.getElementById("spock").onclick = playSpock;
 document.getElementById("reset").onclick = manualScoreReset;
 
-function playRock() {                   //functions to be called to play each choice
-    playGame("rock");
+function playRock() {                   //function created for onclick event to select specific player choice
+    playGame("rock")                  
 }
 
 function playPaper() {
@@ -31,21 +32,25 @@ function playSpock() {
     playGame("spock");
 }
 
-function manualScoreReset() {  
+function manualScoreReset() {                //this function resets both player and shelbot scores to zero and the bout status messages of who played what, winner of round and winner of game.              
     document.getElementById("playerScore").innerHTML = 0;
     document.getElementById("shelBotScore").innerHTML = 0;
     document.getElementById("boutStatus").innerHTML = "";
+    //document.getElementById("results").innerHTML = "";    //is this needed?
+    playerScore = 0;
+    shelBotScore = 0;
+    gameOver = false
 }
 
-function boutStatusMessage(number) {
+function boutStatusMessage(number) {        //function used to insert boutstatus message depending on case of win, lose, or tie
      switch (number) {
         case 1:
             document.getElementById("boutStatus").innerHTML += "<p>You win! :)</p>";
-            playerScore++
+                playerScore++
             break
         case 2:
             document.getElementById("boutStatus").innerHTML += "<p>You lose. :(</p>";
-            shelBotScore++
+                shelBotScore++
             break
         default:
             document.getElementById("boutStatus").innerHTML += "<p>You tied. :|</p>";
@@ -53,22 +58,32 @@ function boutStatusMessage(number) {
      }
 }
 
-function playGame(playerMove) {
-    let result = play(playerMove)
-    boutStatusMessage(result)
-    updateScore()
-    console.log(playerScore)
+function playGame(playerMove) {                         //function 
+    if(!gameOver){
+        let result = play(playerMove)                   //
+        boutStatusMessage(result)                       //displays the bout status message of who won the round
+        updateScore()                                   //updates the score
+        winCheck()                                      //check to see who wins the game
+        console.log(playerScore)
 
-    if(playerScore == 10 || shelBotScore == 10) {
-        displayGameWinner()
+        if(playerScore == 10 || shelBotScore == 10) {   // how to stop the game completely at first to ten
+            displayGameWinner()
+        }
     }
 }
 
-function play(playerMove) {
+// function addToScore(score) {                // is this even needed?
+//     if(score < 10) {
+//         score++
+//     }
+//     return
+// }
+
+function play(playerMove) {                         //function used for player choice and to determine the bout status message and updateing score depending on shelbots move
 
     let shelBotMove = getShelBotMove();
 
-    document.getElementById("boutStatus").innerHTML = "<p>You have selected <strong>" + playerMove + "</strong>. Shelbot has selected <strong>" + shelBotMove + "</strong>.</p>";
+    document.getElementById("boutStatus").innerHTML = "<p>You have selected <strong>" + playerMove + "</strong>. Shelbot has selected <strong>" + shelBotMove + "</strong>.</p>"
 
     if (playerMove == "rock") {
         if (shelBotMove == "rock") {
@@ -133,44 +148,27 @@ function play(playerMove) {
     }
 }
 
-function updateScore() {
-    document.getElementById("playerScore").innerHTML = playerScore;
-    document.getElementById("shelBotScore").innerHTML = shelBotScore;
-}
-
 function getShelBotMove() {                                                 //function to randomize shelbots choice
     let moves = ["rock", "paper", "scissors", "lizard", "spock"];
     let move = moves[Math.floor(Math.random() * moves.length)];    // study this
     return move;
 }
 
-// function winnerOfGame () {
+function updateScore() {                                                    //function to update score in the DOM
+    document.getElementById("playerScore").innerHTML = playerScore;
+    document.getElementById("shelBotScore").innerHTML = shelBotScore;
+}
 
-// }
-
-function displayGameWinner() {               //function display game winner for whoever reaches 10 first// modul?
-    if(playerScore > shelBotScore) {
-        results.innerHTML += "<p>You win the game! :)</p>"
-    } else if(shelBotScore > playerScore) {
-        results.innerHTML += "<p>Bazinga!!!!!  You have lost the game! :(</p>"
+function winCheck() {                                                       //checks to see who won the game based on who reaches the round score of 10 first
+    if(playerScore == 10 || shelBotScore == 10) {
+        gameOver = true
     }
 }
 
-
-// window.location.reload(false);    //add an event listener onclick  to playagain button    div id play again modal
-
-// function endingGameAtScoreOf10() {
-
-// }
-
-//reset at score of 10
-
-//how to exit game/page upon displaying winner of overall game. give option to play again or exit with a next time message?
-
-
-// two classes for modal   id and class
-
-// class=
-
-
-//function that hold score   if score is greater than 10
+function displayGameWinner() {               //function to display overall game winner and game winning message
+    if(playerScore >= shelBotScore) {
+        document.getElementById("boutStatus").innerHTML = "<p><strong>You win the game!</strong> Reset the game if you wish to best Shelbot again! :)</p>"
+    } else if(shelBotScore >= playerScore) {
+        document.getElementById("boutStatus").innerHTML = "<p><strong>Bazinga!!!!!  You have lost the game!</strong> Reset the game if you want to be crushed again!! :(</p>"
+    }
+}
